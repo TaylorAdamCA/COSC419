d34efa7c2e88bc865c39899e48037f1fb9b25348
Taylor Adam <tjadam1993@gmail.com>
2018-11-16 01:18:59 +0000

    New BG Image

--- static/custom.js
+++ static/custom.js
@@ -1,8 +1,8 @@
+// Got this idea from Giulio G. at https://stackoverflow.com/questions/49530497/how-do-i-make-bootstrap-navbar-change-active-state
 $(document).ready(function() {
  var path = window.location.pathname;
  var links = ['home', 'welcome', 'login', 'register'];
 $('.nav-item').each(function(i) {
- console.log(path);
  if (path.includes(links[i])) $(this).addClass('active');
  else if (this.className.includes('active')) $(this).removeClass('active');
 });
